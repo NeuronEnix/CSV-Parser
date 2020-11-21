732018f007295300d0287f2f9127aae63eb1ea12
NEURON <50287102+neuron909@users.noreply.github.com>
2020-11-21 10:39:54 +0530

    Removed: a.hpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include "CSV_Parser/CSV_Parser.hpp"
-#include "a.hpp"
 
 int main() {
 
     CSV_Parser parser = CSV_Parser();
     parser.readFromFile( "file.csv" );
-
-    deb_m( parser.getHeader() );
-    deb_vv( parser.getData() );
         
     return 0;
 }
