3dc2fb3690386b8b0f3644f64b423e83601f465c
NEURON <50287102+neuron909@users.noreply.github.com>
2020-12-04 15:42:06 +0530

    Fix: virtualColPos now holds col positions

--- CSV_Parser/CSV_Data/CSV_Data_Methods/CSV_Data_IO.hpp
+++ CSV_Parser/CSV_Data/CSV_Data_Methods/CSV_Data_IO.hpp
@@ -1,11 +1,11 @@
 #pragma once
+#include<iostream>
 #include "../CSV_Data.hpp"
 #include "../../CSV_Decoder.hpp"
 
 CSV_Data* CSV_Data::readFromFile( std::ifstream& fin ) {
     
     this->data.clear();
-    this->virtualColPos.clear();
     std::string line;
 
     std::vector< std::string > eachRowData;
@@ -14,14 +14,18 @@ CSV_Data* CSV_Data::readFromFile( std::ifstream& fin ) {
         eachRowData.clear();
         CSV_Decoder::decodeToVectorOfString( line, eachRowData );
         this->data.push_back( eachRowData );     
-        this->virtualColPos.push_back( index );
     }
+    
+    // Setting virtualColPos as [0,1,2,3,..., n ] 
+    this->virtualColPos.resize( this->data[0].size() );
+    for (size_t i = 0; i < this->data.size(); i++)
+        this->virtualColPos[ i ] = i;    
+
 }
 
 CSV_Data* CSV_Data::writeToFile( std::ofstream& fout ) {
-    
     for( const auto& eachLine : this->data ) {
-        for( const int ind: this->virtualColPos ) 
+        for( const int& ind: this->virtualColPos ) 
             fout << eachLine[ ind ] << ",";
         fout << std::endl;
     }
